2d7966efe9b2ae761104201a9ca1a901984efbb0
MaskyS <kufahmeeran@gmail.com>
2018-01-02 21:37:56 +0400

    Fix coding standards.

--- src/Plugin/Filter/ImageCaptionFilter.php
+++ src/Plugin/Filter/ImageCaptionFilter.php
@@ -12,7 +12,7 @@ use Drupal\filter\Plugin\FilterBase;
  * in a <div> with the width and class attributes set. A <div> tag of class
  * "caption" is also added with the caption text obtained from the title. It
  * removes the class attribute from the <img> tag.
- * 
+ *
  * @Filter(
  *   id = "image_caption",
  *   title = @Translation("Image Caption"),
@@ -38,7 +38,7 @@ class ImageCaptionFilter extends FilterBase {
       '#description' => $this->t('Enter a space-separated list of classes. The filter will only operate on images which have one of these CSS classes and have a title attribute.'),
       '#required' => TRUE,
     ];
-    
+
     // Ask user if he wants to use Javascript to create the captions.
     $form['javascript_status'] = [
       '#title' => $this->t('Create captions:'),
@@ -57,20 +57,21 @@ class ImageCaptionFilter extends FilterBase {
   }
 
   /**
-  * Implements hook_filter_FILTER_process().
-  */
-  function process($text, $filter, $format, $langcode, $cache, $cache_id) {
+   * Implements hook_filter_FILTER_process().
+   */
+  public function process($text, $filter, $format, $langcode, $cache, $cache_id) {
 
     if ($this->settings['javascript_status'] == 'With Javascript') {
-        
+
       $callback = 'addCaptionWithJavaScript';
 
-    } else {
-      
-      $callback = 'addCaptionWithoutJavaScript'; 
-    
-    } 
- 
+    }
+    else {
+
+      $callback = 'addCaptionWithoutJavaScript';
+
+    }
+
     storeClasses(array_filter(explode(' ', $this->settings['classes'])));
     $text = preg_replace_callback('|(<img.*?>)|s', $callback, $text);
     return new FilterProcessResult($text);
@@ -79,69 +80,80 @@ class ImageCaptionFilter extends FilterBase {
   /**
    * {@inheritdoc}
    */
-  public function tips($long = true) {
-    
-    $js_choice = $this->settings['javascript_status']; 
-    $classes = $this->settings['classes']; 
+  public function tips($long = TRUE) {
+
+    $js_choice = $this->settings['javascript_status'];
+    $classes = $this->settings['classes'];
     $display = js_choice == 'with_js' ? $this->t('by using Javascript.') : $this->t('without using Javascript.');
 
-    $output = $this->t('Adds captions, from the title attribute, to images with one of the following classes: %classes %js_choice', ['%classes' => $classes, '%js_choice' => $js_choice ]) . '<br />';
+    $output = $this->t('Adds captions, from the title attribute, to images with one of the following classes: %classes %js_choice', ['%classes' => $classes, '%js_choice' => $js_choice]) . '<br />';
     return $output;
   }
 
   /**
-  * Storage for active class names.
-  * 
-  * addCaptionWithoutJavaScript() is called by preg_replace_callback() and this function allows only one argument.
-  */
+   * Storage for active class names.
+   *
+   * AddCaptionWithoutJavaScript() is called by preg_replace_callback() and
+   * this function allows only one argument.
+   */
   protected function storeClasses($classes = NULL) {
     static $_classes = [];
-    if ($classes != NULL) { 
-      $_classes = $classes; 
+    if ($classes != NULL) {
+      $_classes = $classes;
     }
 
     return $_classes;
   }
 
+  /**
+   * This will add captions without using Javascript.
+   */
   protected function addCaptionWithoutJavaScript($img_tag_matches, $active_classes = NULL) {
     $img_tag = $img_tag_matches[0];
     $return_text = $img_tag;
-  
-    // Only execute this filter on img tags with (at least) one of the classes we are interested in
+
+    // Only execute this filter on img tags with (at least) one of the classes
+    // we are interested in.
     $has_class = preg_match('/class=\"(.+?)\"/i', $img_tag, $matches) > 0;
     if ($has_class) {
       $class = $matches[1];
-      // Formally, class is a space separated list of classes, but we allow all horizontal whitespace in any quantity.
-      // That's why we use preg_split instead of explode.
-      $classes = preg_split('/\s+/', $class, null, PREG_SPLIT_NO_EMPTY);
-  
-      // Get active classes via storeClasses() because preg_replace_callback() does not support addional arguments.
+
+      /*
+       * Formally, class is a space separated list of classes, but we allow all
+       * horizontal whitespace in any quantity. That's why we use preg_split
+       * instead of explode.
+       */
+      $classes = preg_split('/\s+/', $class, NULL, PREG_SPLIT_NO_EMPTY);
+
+      // Get active classes via storeClasses() because preg_replace_callback()
+      // does not support addional arguments.
       if ($active_classes == NULL) {
         $active_classes = storeClasses();
       }
-  
+
       if (count(array_intersect($classes, $active_classes)) > 0) {
         // Only execute this filter on img tags that have a title attribute.
         $has_title = preg_match('/title=\"(.+?)\"/i', $img_tag, $matches) > 0;
         if ($has_title) {
           $title = $matches[1];
-  
+
           // Search for width specified as an inline style or width attribute,
-          // if no width specified, don't output it on the outer span, assume width will be handled with css external to this module/filter.
+          // if no width specified, don't output it on the outer span, assume
+          // width will be handled with css external to this module/filter.
           $width = '';
-          if (preg_match('/width:\s*(\d+)px/i', $img_tag, $matches) == 1 || preg_match ('/width=\"(\d+?)\"/i', $img_tag, $matches) == 1) {
+          if (preg_match('/width:\s*(\d+)px/i', $img_tag, $matches) == 1 || preg_match('/width=\"(\d+?)\"/i', $img_tag, $matches) == 1) {
             $width = $matches[1];
           }
-  
+
           // Search for float specified as an inline style on the image.
           $float = '';
           if (preg_match('/float:\s*(\w+)/i', $img_tag, $matches) == 1) {
             $float = $matches[1];
           }
-  
+
           // Remove the class from the image tag.
           $img_tag = preg_replace('/class=\"(.+?)\"/i', '', $img_tag);
-  
+
           // Build the image and caption.
           $caption = [
             'img' => [
@@ -158,33 +170,39 @@ class ImageCaptionFilter extends FilterBase {
               '#value' => $title,
             ],
           ];
-  
+
           // Build the wrapping elemement.
           $element = [
             'image_caption' => [
               '#type' => 'html_tag',
               '#tag' => 'span',
               '#attributes' => [
-                'class' => $class
+                'class' => $class,
               ],
               '#value' => render($caption),
             ],
           ];
-  
+
           if (!empty($width)) {
             $element['image_caption']['#attributes']['style'][] = 'width:' . $width . 'px;';
           }
-          
+
           if (!empty($float)) {
             $element['image_caption']['#attributes']['style'][] = 'float:' . $float;
           }
-          
+
           $return_text = render($element);
         }
       }
     }
   }
+
+  /**
+   * This will use Javascript to add the functions. Here we will simply attach
+   * the library containing the JS.
+   */
   protected function addCaptionWithJavaScript() {
     $form['#attached']['library'][] = 'image_caption/image_caption';
   }
+
 }
